07013c3eb5c2064a29dd2fa0c6f00a81debf6d19
Maria Podolina <masapodolina@icloud.com>
2026-05-15 10:30:18 +0300

    fix endpoints __init__.py

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -1,7 +1,1 @@
-from serializers import AddEmployee, AddOU, UpdateEmployeeStatus, ChangeEmployeeParams, ChangePassword, AppendGroupOfStuff, AddOrDeleteGroupMember, ChangesAMAccountName, Add1cId, PrintMembers
-from service_interaction import check_base_dn, create_new_ou, search_all_user_params, add_new_information, activate_employee_status, disable_emplooyee_status, change_params, update_password, change_employee_in_current_group, update_sAMAccountName, search_user_info, add_id, print_group_members
-from functions import check_headers
-from logger import logger
-from dependencies import connecter
-
-from .ad_api import router
+from .employee import router
